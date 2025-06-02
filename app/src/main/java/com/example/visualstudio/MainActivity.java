5ba55a9ecf34e8dd4d0c1b5a69ae154170dafc6d
AkitaPTS <vendiedonn@gmail.com>
2025-06-02 15:39:06 +0300

    Initial commit

---
+++ app/src/main/java/com/example/visualstudio/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.visualstudio;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        loadFragment(new StartFragment());
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
+    }
+}
