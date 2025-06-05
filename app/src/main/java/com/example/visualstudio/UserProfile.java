f8c3a763001f8b2a7cd8da8c7cefea990133b45f
AkitaPTS <vendiedonn@gmail.com>
2025-06-05 23:31:24 +0300

    Start fragment update

--- app/src/main/java/com/example/visualstudio/UserProfile.java
+++ app/src/main/java/com/example/visualstudio/UserProfile.java
@@ -2,9 +2,6 @@ package com.example.visualstudio;
 
 public class UserProfile {
     public String email;
-    // добавляйте другие поля, например:
-    // public String displayName;
-    // public String avatarUrl;
 
     // Конструктор без аргументов обязателен для Firebase
     public UserProfile() { }
